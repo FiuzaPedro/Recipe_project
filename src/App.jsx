182ab694a91dd8cc66bbca8f8c9f63ac6e31a0f9
Fiuza <pedro_fiuza@hotmail.com>
2026-03-10 10:10:01 +0000

    Added favicon receitas, added the json file with the recipes and added the useEffect hook to get data (testing) in the main app.jsx file

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,20 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
   const [count, setCount] = useState(0)
+  useEffect(() => {
+    fetch('/src/receitas.json')
+      .then(response => response.json())
+      .then(data => {
+        console.log(data);  
+      })
+      .catch(error => {
+        console.log('Error:', error);  
+      });
+  }, []); 
 
   return (
     <>
